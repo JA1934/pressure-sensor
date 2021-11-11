9c7ad96c0466a2232a463cdbe003b1d8a586aefc
JA1934 <92798990+JA1934@users.noreply.github.com>
2021-11-11 08:49:14 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 let value = 0
+// Make the pressure sensor at pin 0 and show how much pressure is on it.
 basic.forever(function () {
     value = pins.analogReadPin(AnalogPin.P0)
     basic.showNumber(value)
